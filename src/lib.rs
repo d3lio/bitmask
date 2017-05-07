31bfe463eb24659d09dfa09fcd5aa341d3cb6317
Delyan Dobrev <delqn.dobrev@gmail.com>
2017-05-07 13:45:32 +0300

    Allow no_std
    * `contains` and `intersects` take immutable refs now.
    * Moved doc to crate level and listed all methods and operators.
    
    Tried to simplify the macro parsing but the local ambiguity still
    persists.

--- src/lib.rs
+++ src/lib.rs
@@ -1,160 +1,197 @@
 //! A bitmask generator for enum scoped bit flags.
+//!
+//! The `bitmask!` macro creates a struct and an enum that holds your flags. The enum contains all the
+//! bit flag variants and the struct is a mixture of those bit flags called a bitmask.
+//! It's syntax is as follows:
+//!
+//! ```ignore
+//! bitmask! {
+//!     pub mask <struct_name>: <struct_type> where flags <enum_name> {
+//!         <flag_name> = <value>,
+//!         ...
+//!     }
+//! }
+//! ```
+//!
+//! where `pub` is optional and `struct_type` can be one of the primitive integer types
+//! (`i8-64`, `u8-64`, `isize`, `usize`).
+//!
+//! # Application
+//!
+//! Sometimes you might want to wrap some lib that ports `C` or some other code through FFI
+//! which exposes numerous defines/constants as `const`. Lets take a look at this example module:
+//!
+//! ```ignore
+//! mod tex {
+//!     ...
+//!     pub const TEXTURE_2D: u32   = 1;
+//!     pub const TEXTURE_3D: u32   = 2;
+//!     pub const FLIP: u32         = 4;
+//!     ...
+//!     pub fn set_options(mask: u32) { ... }
+//! }
+//! ```
+//!
+//! To avoid collisions you would use these through the mod scope like so:
+//!
+//! ```ignore
+//! tex::set_options(tex::TEXTURE_2D | tex::FLIP);
+//! ```
+//!
+//! But that does not guarantee you that you won't use invalid flag values.
+//! For example you could do:
+//!
+//! ```ignore
+//! set_options(3 | 8);
+//! ```
+//!
+//! Now imagine you had an enum to hold all of those flags and a common type that does not accept
+//! any types other than the enum variants and itself. This is exactly what `bitmask!` does for you!
+//! It generates an enum with the variants (flags) you supply and a struct that
+//! holds a mask which is a mixture of these variants. So now our example would look like this:
+//!
+//! ```
+//! # mod tex {
+//! #     pub const TEXTURE_2D: u32   = 1;
+//! #     pub const TEXTURE_3D: u32   = 2;
+//! #     pub const FLIP: u32         = 4;
+//! #     pub fn set_options(mask: u32) {}
+//! # }
+//! # #[macro_use] extern crate bitmask; fn main() {
+//! bitmask! {
+//!     pub mask TexMask: u32 where flags TexOption {
+//!         Texture2d = tex::TEXTURE_2D,
+//!         Texture3d = tex::TEXTURE_3D,
+//!         Flip = tex::FLIP
+//!     }
+//! }
+//!
+//! fn set_options(mask: TexMask) {
+//!     tex::set_options(*mask);
+//! }
+//!
+//! // Single flag
+//! set_options(TexOption::Texture2d.into());
+//! set_options(TexMask::from(TexOption::Texture3d));
+//!
+//! // Multiple flags
+//! set_options(TexOption::Texture2d | TexOption::Flip);
+//! # }
+//! ```
+//!
+//! # Things that are doable but _can change_ with time:
+//!
+//! If for some reason you want to define the enum and the struct yourself you can do so and use the
+//! `@IMPL` branch of the macro to implement the methods. The only restrictions are that your
+//! struct's inner field must be named `mask` and the enum should have the same size as the struct
+//! which can be achieved through the `#[repr()]` modifier with the same integer type as the field `mask`.
+//!
+//! Implementing `Into<struct_name>` and `Deref` for your own custom type is possible if you want to
+//! use it with the preimplemented methods for the mask but does not apply for the trait implements
+//! like `BitOr` for example.
+//!
+//! # Examples:
+//!
+//! ```
+//! # #[macro_use] extern crate bitmask; fn main() {
+//! bitmask! {
+//!     mask BitMask: u32 where flags Flags {
+//!         Flag1       = 0x00000001,
+//!         Flag2       = 0x000000F0,
+//!         Flag3       = 0x00000800,
+//!         Flag123     = 0x000008F1,
+//!         // Note that function calls like `isize::min_value()`
+//!         // can't be used for enum discriminants in Rust.
+//!         FlagMax     = ::std::u32::MAX
+//!     }
+//! }
+//!
+//! let mut mask = BitMask::none();
+//!
+//! mask.set(Flags::Flag1 | Flags::Flag2);
+//! assert_eq!(*mask, 0x000000F1);
+//!
+//! mask.unset(Flags::Flag1);
+//! assert_eq!(*mask, 0x000000F0);
+//!
+//! mask.set(Flags::Flag123);
+//! assert_eq!(*mask, 0x000008F1);
+//! # }
+//! ```
+//!
+//! You can add meta attributes like documentation (`#[doc = "..."]`) to each element of the macro:
+//!
+//! ```
+//! # #[macro_use] extern crate bitmask; fn main() {
+//! bitmask! {
+//!     /// Doc comment for the struct
+//!     pub mask SomeOtherMask: isize where
+//!     /// Doc comment for the enum
+//!     flags SomeOtherFlags {
+//!         /// Doc comment for the flag
+//!         FlagZero    = 0,
+//!         FlagOne     = 1
+//!     }
+//! }
+//! # }
+//! ```
+//!
+//! Maybe not the best example but still... Cake is love!
+//!
+//! ```
+//! # #[macro_use] extern crate bitmask; fn main() {
+//! bitmask! {
+//!     mask Cake: u8 where flags Ingredients {
+//!         Sugar   = 0b00000001,
+//!         Eggs    = 0b00000010,
+//!         Flour   = 0b00000100,
+//!         Milk    = 0b00001000
+//!     }
+//! }
+//!
+//! let quality_cake = Cake::all();
+//! assert_eq!(*quality_cake, 0b00001111);
+//! # }
+//! ```
 
-/// Macro that generates a bitmask and it's associated bit flags.
+#![no_std]
+
+// Re-export libcore using an alias so that the macro can work in `no_std` crates while remaining
+// compatible with normal crates
+#[doc(hidden)]
+pub extern crate core as __core;
+
+/// The macro that generates a bitmask and it's associated bit flags.
 ///
-/// The `bitmask!` macro creates a struct and an enum that holds your flags. The enum contains all the
-/// bit flag variants and the struct is a mixture of those bit flags called a bitmask.
-/// It's syntax is as follows:
+/// See the crate level docs for more info and examples.
 ///
-/// ```ignore
-/// bitmask! {
-///     pub mask <struct_name>: <struct_type> where flags <enum_name> {
-///         <flag_name> = <value>,
-///         ...
-///     }
-/// }
-/// ```
+/// # Methods
 ///
-/// where `pub` is optional and `struct_type` can be one of the primitive integer types
-/// (`i8-64`, `u8-64`, `isize`, `usize`).
+/// * `none` - Create a new mask with all flags unset.
+/// * `all` - Create a new mask with all flags set.
+/// * `set` - Set a single flag if enum flag variant is passed or multiple if mask is passed.
+/// * `unset` - Unset a single flag if enum flag variant is passed or multiple if mask is passed.
+/// * `toggle` - Same as set/unset but always negates the flags (1 -> 0 and 0 -> 1).
+/// * `contains` - Check if the mask contains a flag or a whole mask.
+/// * `intersects` - Check if the mask intersects with a flag or a whole mask.
+/// * `is_all` - Check if all flag variants are set.
+/// * `is_none` - Check if all flag variants are unset.
 ///
-/// # Application
+/// # Operators
 ///
-/// Sometimes you might want to wrap some lib that ports `C` or some other code through FFI
-/// which exposes numerous defines/constants as `const`. Lets take a look at this example module:
+/// * `BitOr`
+/// * `BitXor`
+/// * `BitAnd`
+/// * `BitOrAssign`
+/// * `BitXorAssign`
+/// * `BitAndAssign`
+/// * `Not` - Same as toggle but creates a new mask with the resulting value.
 ///
-/// ```ignore
-/// mod tex {
-///     ...
-///     pub const TEXTURE_2D: u32   = 1;
-///     pub const TEXTURE_3D: u32   = 2;
-///     pub const FLIP: u32         = 4;
-///     ...
-///     pub fn set_options(mask: u32) { ... }
-/// }
-/// ```
+/// # Misc
 ///
-/// To avoid collisions you would use these through the mod scope like so:
-///
-/// ```ignore
-/// tex::set_options(tex::TEXTURE_2D | tex::FLIP);
-/// ```
-///
-/// But that does not guarantee you that you won't use invalid flag values.
-/// For example you could do:
-///
-/// ```ignore
-/// set_options(3 | 8);
-/// ```
-///
-/// Now imagine you had an enum to hold all of those flags and a common type that does not accept
-/// any types other than the enum variants and itself. This is exactly what `bitmask!` does for you!
-/// It generates an enum with the variants (flags) you supply and a struct that
-/// holds a mask which is a mixture of these variants. So now our example would look like this:
-///
-/// ```
-/// # mod tex {
-/// #     pub const TEXTURE_2D: u32   = 1;
-/// #     pub const TEXTURE_3D: u32   = 2;
-/// #     pub const FLIP: u32         = 4;
-/// #     pub fn set_options(mask: u32) {}
-/// # }
-/// # #[macro_use] extern crate bitmask; fn main() {
-/// bitmask! {
-///     pub mask TexMask: u32 where flags TexOption {
-///         Texture2d = tex::TEXTURE_2D,
-///         Texture3d = tex::TEXTURE_3D,
-///         Flip = tex::FLIP
-///     }
-/// }
-///
-/// fn set_options(mask: TexMask) {
-///     tex::set_options(*mask);
-/// }
-///
-/// // Single flag
-/// set_options(TexOption::Texture2d.into());
-/// set_options(TexMask::from(TexOption::Texture3d));
-///
-/// // Multiple flags
-/// set_options(TexOption::Texture2d | TexOption::Flip);
-/// # }
-/// ```
-///
-/// # Things that are doable but _can change_ with time:
-///
-/// If for some reason you want to define the enum and the struct yourself you can do so and use the
-/// `@IMPL` branch of the macro to implement the methods. The only restrictions are that your
-/// struct's inner field must be named `mask` and the enum should have the same size as the struct
-/// which can be achieved through the `#[repr()]` modifier with the same integer type as the field `mask`.
-///
-/// Implementing `Into<struct_name>` and `Deref` for your own custom type is possible if you want to
-/// use it with the preimplemented methods for the mask but does not apply for the trait implements
-/// like `BitOr` for example.
-///
-/// # Examples:
-///
-/// ```
-/// # #[macro_use] extern crate bitmask; fn main() {
-/// bitmask! {
-///     mask BitMask: u32 where flags Flags {
-///         Flag1       = 0x00000001,
-///         Flag2       = 0x000000F0,
-///         Flag3       = 0x00000800,
-///         Flag123     = 0x000008F1,
-///         // Note that function calls like `isize::min_value()`
-///         // can't be used for enum discriminants in Rust.
-///         FlagMax     = ::std::u32::MAX
-///     }
-/// }
-///
-/// let mut mask = BitMask::none();
-///
-/// mask.set(Flags::Flag1 | Flags::Flag2);
-/// assert_eq!(*mask, 0x000000F1);
-///
-/// mask.unset(Flags::Flag1);
-/// assert_eq!(*mask, 0x000000F0);
-///
-/// mask.set(Flags::Flag123);
-/// assert_eq!(*mask, 0x000008F1);
-/// # }
-/// ```
-///
-/// You can add meta attributes like documentation (`#[doc = "..."]`) to each element of the macro:
-///
-/// ```
-/// # #[macro_use] extern crate bitmask; fn main() {
-/// bitmask! {
-///     /// Doc comment for the struct
-///     pub mask SomeOtherMask: isize where
-///     /// Doc comment for the enum
-///     flags SomeOtherFlags {
-///         /// Doc comment for the flag
-///         FlagZero    = 0,
-///         FlagOne     = 1
-///     }
-/// }
-/// # }
-/// ```
-///
-/// Maybe not the best example but still... Cake is love!
-///
-/// ```
-/// # #[macro_use] extern crate bitmask; fn main() {
-/// bitmask! {
-///     mask Cake: u8 where flags Ingredients {
-///         Sugar   = 0b00000001,
-///         Eggs    = 0b00000010,
-///         Flour   = 0b00000100,
-///         Milk    = 0b00001000
-///     }
-/// }
-///
-/// let quality_cake = Cake::all();
-/// assert_eq!(*quality_cake, 0b00001111);
-/// # }
-/// ```
+/// * `From`< _enum_ > for _struct_
+/// * `Deref` for _struct_
+/// * `Deref` for _enum_
 // TODO: simplify the parsing when https://github.com/rust-lang/rust/issues/24189 is resolved
 #[macro_export]
 macro_rules! bitmask {
@@ -506,7 +543,7 @@ macro_rules! bitmask {
             /// `other` can be either a single flag or another mask.
             #[inline]
             pub fn set<T>(&mut self, other: T)
-                where T: Into<$st_name> + ::std::ops::Deref<Target = $T> {
+                where T: Into<$st_name> + $crate::__core::ops::Deref<Target = $T> {
                     self.mask |= *other;
             }
 
@@ -515,7 +552,7 @@ macro_rules! bitmask {
             /// `other` can be either a single flag or another mask.
             #[inline]
             pub fn unset<T>(&mut self, other: T)
-                where T: Into<$st_name> + ::std::ops::Deref<Target = $T> {
+                where T: Into<$st_name> + $crate::__core::ops::Deref<Target = $T> {
                     self.mask &= Self::all().mask ^ *other;
             }
 
@@ -524,7 +561,7 @@ macro_rules! bitmask {
             /// `other` can be either a single flag or another mask.
             #[inline]
             pub fn toggle<T>(&mut self, other: T)
-                where T: Into<$st_name> + ::std::ops::Deref<Target = $T> {
+                where T: Into<$st_name> + $crate::__core::ops::Deref<Target = $T> {
                     self.mask ^= *other;
             }
 
@@ -532,8 +569,8 @@ macro_rules! bitmask {
             ///
             /// `other` can be either a single flag or another mask.
             #[inline]
-            pub fn contains<T>(&mut self, other: T) -> bool
-                where T: Into<$st_name> + ::std::ops::Deref<Target = $T> {
+            pub fn contains<T>(&self, other: T) -> bool
+                where T: Into<$st_name> + $crate::__core::ops::Deref<Target = $T> {
                     self.mask & *other == *other
             }
 
@@ -541,8 +578,8 @@ macro_rules! bitmask {
             ///
             /// `other` can be either a single flag or another mask.
             #[inline]
-            pub fn intersects<T>(&mut self, other: T) -> bool
-                where T: Into<$st_name> + ::std::ops::Deref<Target = $T> {
+            pub fn intersects<T>(&self, other: T) -> bool
+                where T: Into<$st_name> + $crate::__core::ops::Deref<Target = $T> {
                     self.mask & *other != 0
             }
 
@@ -551,13 +588,13 @@ macro_rules! bitmask {
                 self.mask == Self::all().mask
             }
 
-            /// Check if all flags are set.
+            /// Check if all flags are unset.
             pub fn is_none(&self) -> bool {
                 self.mask == 0
             }
         }
 
-        impl ::std::convert::From<$en_name> for $st_name {
+        impl $crate::__core::convert::From<$en_name> for $st_name {
             /// Create a mask from a single flag.
             ///
             /// When creating a mask from multiple flags or another mask just use the `clone` method
@@ -569,7 +606,7 @@ macro_rules! bitmask {
                 }
             }
         }
-        impl ::std::ops::Deref for $st_name {
+        impl $crate::__core::ops::Deref for $st_name {
             type Target = $T;
             /// Deref to the internal type.
             ///
@@ -579,14 +616,14 @@ macro_rules! bitmask {
                 &self.mask as &$T
             }
         }
-        impl ::std::ops::Deref for $en_name {
+        impl $crate::__core::ops::Deref for $en_name {
             type Target = $T;
             /// Deref to the internal type.
             ///
             /// Useful for FFI.
             #[inline]
             fn deref(&self) -> &$T {
-                unsafe { ::std::mem::transmute(self) }
+                unsafe { $crate::__core::mem::transmute(self) }
             }
         }
 
@@ -630,7 +667,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_BITOR $($target: ty, $other: ty, $st_name: ident);*;) => {
-        $(impl ::std::ops::BitOr<$other> for $target {
+        $(impl $crate::__core::ops::BitOr<$other> for $target {
             type Output = $st_name;
             #[inline]
             fn bitor(self, other: $other) -> Self::Output {
@@ -642,7 +679,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_BITAND $($target: ty, $other: ty, $st_name: ident);*;) => {
-        $(impl ::std::ops::BitAnd<$other> for $target {
+        $(impl $crate::__core::ops::BitAnd<$other> for $target {
             type Output = $st_name;
             #[inline]
             fn bitand(self, other: $other) -> Self::Output {
@@ -654,7 +691,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_BITXOR $($target: ty, $other: ty, $st_name: ident);*;) => {
-        $(impl ::std::ops::BitXor<$other> for $target {
+        $(impl $crate::__core::ops::BitXor<$other> for $target {
             type Output = $st_name;
             #[inline]
             fn bitxor(self, other: $other) -> Self::Output {
@@ -666,7 +703,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_BITOR_ASSIGN $($target: ty, $other: ty);*;) => {
-        $(impl ::std::ops::BitOrAssign<$other> for $target {
+        $(impl $crate::__core::ops::BitOrAssign<$other> for $target {
             #[inline]
             fn bitor_assign(&mut self, other: $other) {
                 self.mask |= *other
@@ -675,7 +712,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_BITAND_ASSIGN $($target: ty, $other: ty);*;) => {
-        $(impl ::std::ops::BitAndAssign<$other> for $target {
+        $(impl $crate::__core::ops::BitAndAssign<$other> for $target {
             #[inline]
             fn bitand_assign(&mut self, other: $other) {
                 self.mask &= *other
@@ -684,7 +721,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_BITXOR_ASSIGN $($target: ty, $other: ty);*;) => {
-        $(impl ::std::ops::BitXorAssign<$other> for $target {
+        $(impl $crate::__core::ops::BitXorAssign<$other> for $target {
             #[inline]
             fn bitxor_assign(&mut self, other: $other) {
                 self.mask ^= *other
@@ -693,7 +730,7 @@ macro_rules! bitmask {
     };
 
     (@IMPL_NOT $($target: ty, $st_name: ident);*;) => {
-        $(impl ::std::ops::Not for $target {
+        $(impl $crate::__core::ops::Not for $target {
             type Output = $st_name;
             #[inline]
             fn not(self) -> Self::Output {
@@ -708,6 +745,8 @@ macro_rules! bitmask {
 
 #[cfg(test)]
 mod tests {
+    extern crate std;
+
     bitmask! {
         /// Doc comment
         mask BitMask: isize where
@@ -717,7 +756,7 @@ mod tests {
             /// Doc comment
             Flag2      = 0b00000010,
             Flag3      = 0b00000100,
-            FlagMin    = ::std::isize::MIN,
+            FlagMin    = std::isize::MIN,
             /// Doc comment
             Flag123    = 0b00000111
         }
@@ -769,7 +808,7 @@ mod tests {
 
     #[test]
     fn test_intersects() {
-        let mut bm = BitMask::from(Flag2 | Flag3);
+        let bm = BitMask::from(Flag2 | Flag3);
         assert_eq!(bm.intersects(Flag1), false);
         assert_eq!(bm.intersects(Flag2), true);
         assert_eq!(bm.intersects(Flag3), true);
